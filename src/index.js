f2575ad4fa3a145959676e75a942abfc4c22880c
Oleksandr Shumylo <ashumylo@ukr.net>
2022-08-22 18:43:58 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.html';
+import './css/style.css';
+
+
+const mult = (a, b) => a * b;
+console.log(mult(2, 4));
+console.log(mult(3, 4));
---
+++ src/js/slider.js
@@ -0,0 +1,22 @@
+	var swiper = new Swiper(".mySwiper", {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		slidesPerGroup: 1,
+		breakpoints: {
+		 768: {
+				slidesPerView: 2,
+			},
+
+			991: {
+				slidesPerView: 5,
+			},
+		},
+		loop: true,
+		navigation:{
+			nextEl:".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		}
+
+
+	});
+
